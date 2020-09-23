b8bf0e7b79c525131eef8b1a3127715ac042b619
msolorio <msolorio789@gmail.com>
2020-09-22 23:18:31 -0700

    multiple pets rendering with stats and updating stats

--- js/app.js
+++ js/app.js
@@ -12,11 +12,13 @@ CURRENTLY ALIVE AND HOW LONG THEY HAVE BEEN ALIVE
 */
 const $pets = $('#pets');
 const $createNewPetButton = $('#create-new-pet');
+const $createPetInput = $('#pet-input');
 
 // - CREATE 1 TOMAGOTCHI CLASS
 class Pet {
-  constructor(petName, hunger=10, sleepiness=10, boredom=10) {
+  constructor(petName, petId, hunger=0, sleepiness=0, boredom=0) {
     this.petName = petName;
+    this.petId = petId;
     this.hunger = hunger;
     this.sleepiness = sleepiness;
     this.boredom = boredom;
@@ -35,42 +37,125 @@ class Game {
   constructor() {
     this.score = 0,
     this.totalTime = 0
-    this.pets = []
+    this.pets = [],
+    this.indexCount = 0
   }
 
-  // FORM
+  // DATA
+  // TODO: move to Pet class
+  resetPetStat(petId, stat) {
+    const petToUpdate = this.pets.find((pet) => pet.petId === petId);
+    petToUpdate[stat] = 0;
+    this.renderAllPetsStats(this.pets);
+  }
+
+  // TODO: move to Pet class
+  decrementPetStats(pet) {
+    pet.hunger += 1;
+    pet.sleepiness += 1;
+    pet.boredom += 1;
+  }
+
+  decrementAllPetsStats(allPets) {
+    allPets.forEach((pet) => {
+      this.decrementPetStats(pet);
+    })
+  }
+
+  renderAllPetsStats(allPets) {
+    allPets.forEach((pet) => {
+      $(`#pet-hunger-${pet.petId}`).text(pet.hunger);
+      $(`#pet-sleepiness-${pet.petId}`).text(pet.sleepiness);
+      $(`#pet-boredom-${pet.petId}`).text(pet.boredom);
+    });
+  }
+
+  setTimer() {
+    const gameTimer = window.setInterval(() => {
+      this.decrementAllPetsStats(this.pets);
+      this.renderAllPetsStats(this.pets);
+    }, 3000);
+  }
+
   startGame() {
-    const that = this;
-    
-    $createNewPetButton.on('click', function() {
+    $createNewPetButton.on('click', () => {
       // if this is first pet start timer
+      !this.pets.length && this.setTimer();
 
       // calls createPet passing in name from input field
+      const petName = $createPetInput.val().trim();
+      petName && this.createPet(petName);
+    });
 
-      that.createPet('testName');
+    $pets.on('click', '.pet-feed', (event) => {
+      const petId = $(event.target).data('petid');
+      this.resetPetStat(petId, 'hunger');
+    });
+
+    $pets.on('click', '.pet-play', (event) => {
+      const petId = $(event.target).data('petid');
+      this.resetPetStat(petId, 'boredom');
     });
   }
 
   createPet(petName) {
-    const newPet = new Pet(petName);
+    const newPet = new Pet(petName, this.indexCount);
 
     this.pets.push(newPet);
 
-    console.log('this:', this);
+    this.indexCount += 1;
+
+    console.log('this.pets:', this.pets);
+
+    this.renderPet(newPet);
   }
 
   // RENDER
-  renderTomagotchi(petName, petId) {
-    const tomagotchiMarkup = (`
-    <div class="pet" id="${petId}">
-      <h3 class="pet-name">${petName}</h3>
-      <img class="pet-img" src="./images/snail1.jpg" alt="pet" />
+  // TODO: move to Pet class
+  getPetMarkup(petData) {
+    return (`
+      <div class="pet" id="${petData.petId}">
+        <h3 class="pet-name">${petData.petName}</h3>
+        <img class="pet-img" src="./images/snail1.jpg" alt="pet" />
+        <div class="pet-status-group">
+          <span class="pet-status">
+            <p>Hunger:</p>
+            <p id="pet-hunger-${petData.petId}">${petData.hunger}</p>
+          </span>
+          <span class="pet-status">
+            <p>Sleepiness:</p>
+            <p id="pet-sleepiness-${petData.petId}">${petData.sleepiness}</p>
+          </span>
+          <span class="pet-status">
+            <p>Boredom:</p>
+            <p id="pet-boredom-${petData.petId}">${petData.boredom}<p/>
+          </span>
+        </div>
+        <div class="pet-interactions">
+          <button
+            class="pet-interact-button pet-feed"
+            data-petid=${petData.petId}>
+            Feed
+          </button>
+          <button
+            class="pet-interact-button pet-play"
+            data-petid=${petData.petId}>
+            Play
+          </button>
+        </div>
       </div>
     `);
-
-    $pets.html(tomagotchiMarkup);  
   }
+
+  // TODO: move to Pet class
+  renderPet(petData) {
+    this.pets.length === 1 && $pets.html('');
+
+    $pets.append(this.getPetMarkup(petData));
+  }
+
 }
 
 const game = new Game();
 game.startGame();
+
